8a14e44381b08b2c6fdf80a2005a518ba2c9b4f7
arun980798 <arunrajput88176894@gmail.com>
2025-12-19 10:26:11 +0530

    show todo list done

--- script.js
+++ script.js
@@ -1,40 +1,112 @@
-function openclosecard(){
+let form = document.querySelector('#forme')
+let input = document.querySelector(' #forme input');
+let textarea = document.querySelector(' #forme textarea')
+let taskceckbox = document.querySelector('.markimp input')
+let currenttask = [
+  {
+    task: "mandir jao",
+    details: "subja jana h ",
+    imp: true,
+  },
+  {
+    task: "padne  jao",
+    details: "dophar me  jana h ",
+    imp: false,
+  },
+  {
+    task: "ghumne  jao",
+    details: "sham ko  jana h ",
+    imp: true,
+  }
+]
+
+
+
+//use to open close task 
+function openclosecard() {
   const alelement = document.querySelectorAll('.elem')//it give the all element in the form of nodelist 
-const fullblock = document.querySelectorAll('.fullelem'); //it give full block nodelist 
-const back = document.querySelectorAll(".backo")
+  const fullblock = document.querySelectorAll('.fullelem'); //it give full block nodelist 
+  const back = document.querySelectorAll(".backo")
 
 
 
 
 
 
-//it select all element 
-//to open full screen page 
-alelement.forEach(function (ele) {
-  //it do click opration where we click 
-  //ye lisner kam karega jispe click keya h usko handel karega 
-  ele.addEventListener('click', function () {
+  //it select all element 
+  //to open full screen page 
+  alelement.forEach(function (ele) {
+    //it do click opration where we click 
+    //ye lisner kam karega jispe click keya h usko handel karega 
+    ele.addEventListener('click', function () {
 
-  fullblock[ele.id].style.display = "block"
-  })
-})
-
-
-
-//to close full screen and go back
-back.forEach(function(eleo){
-  console.log(eleo)
-  eleo.addEventListener("click",function(){
-    fullblock[eleo.id].style.display = "none"
-
+      fullblock[ele.id].style.display = "block"
+    })
   })
 
 
-})
+
+  //to close full screen and go back
+  back.forEach(function (eleo) {
+
+    eleo.addEventListener("click", function () {
+      fullblock[eleo.id].style.display = "none"
+
+    })
+
+
+  })
 }
-
-
 openclosecard()
 
 
+//use to render todo list 
+function rendertask(){
+  
+
+  let allshowtask = document.querySelector('.showtodo')
+  
+  
+  let sum = ' '
+  
+  currenttask.forEach(function (ele) {
+    sum = sum + ` <div class="taskso  ">
+     <div id="left">
+      <h2  class="">${ele.task} <span class="${ele.imp}">imp </span></h2>
+       <p>${ele.details}</p>
+     </div>
+   <button>remove</button>
+  
+  </div>`
+  
+  })
+  allshowtask.innerHTML = sum;
+  
+  }
+  rendertask();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener("submit", function (eme) {
+  eme.preventDefault();
+  // console.log(form)
+  // console.log(input.value)
+  // console.log(textarea.value)
+  console.log(taskceckbox.checked)
+
+})
 
